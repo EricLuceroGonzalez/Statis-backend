5579cab0ac719cf5ed91f2ad20867132568c65f5
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2020-02-01 18:51:52 -0500

    changed cors call

--- server/index.js
+++ server/index.js
@@ -13,21 +13,15 @@ const port = process.env.PORT || 8000;
 var dotenv = require("dotenv");
 dotenv.config();
 
-
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  // Request headers you wish to allow
-  // res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
- res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  // Pass to next layer of middleware
-  next();
-});
+const corsOptions = {
+  origin: "https://your-app-name.herokuapp.com",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+app.options("*", cors());
 
 // Use CORS
-app.use(cors());
+// app.use(cors());
 
 // ----------- BODY PARSER  ---------------
 var bodyParser = require("body-parser"); // for parsing application/json
